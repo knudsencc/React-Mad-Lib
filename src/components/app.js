86907717dd43a7c7467d827cc652c372c3726b71
Cameron KNUDSEN <tmphotock@gmail.com>
2018-05-03 17:22:08 -0600

    merging repository

---
+++ src/components/app.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import MadlibForm from './madlib_form';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='backgroundSkew'>
+          <div className="madlib-heading">
+            <h1>Mad Libs</h1>
+            <div className="madlib-subheading">
+            Fill out the fields below and click the generate button <br/>to see the Mad Libs story.
+            </div>
+          </div>
+          <MadlibForm />
+
+        </div>
+      </div>
+    );
+  }
+} 
